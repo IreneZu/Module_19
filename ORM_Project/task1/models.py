0b462dfd971b47b1cdff05b8537a11c07d8a8c9c
ZBook <izuraeva@gmail.com>
2025-01-03 23:30:30 +0300

    Mod_19_ex_2

--- ORM_Project/task1/models.py
+++ ORM_Project/task1/models.py
@@ -26,5 +26,16 @@ class Game(models.Model):
 Buyer.objects.create(name= 'Serge', balance = 1500.05, age = 22)
 Buyer.objects.create(name= 'Rosaria', balance = 42.15, age = 62)
 Buyer.objects.create(name= 'Rasor', balance = 0.5, age = 12)
- 
+
+Game.objects.create(title= 'Atomic Heart', cost= 31, size= 46.2, 
+    description='The alternative reality of the 1950s', age_limited=1)
+Game.objects.create(title= 'Farmington', cost= 10, size=0.5, 
+    description='You are a farmer', age_limited=0)
+Game.objects.create(title= 'Subnautica', cost= 15, size= 36, 
+    description='Underwater world', age_limited=1)
+
+Game.objects.get(id=1).buyer.set((1,2)) 
+Game.objects.get(id=2).buyer.set((2,3)) 
+Game.objects.get(id=3).buyer.set((1,2)) 
+
 '''
