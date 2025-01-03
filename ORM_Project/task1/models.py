8cd3bf5497f2284c79cf4bc5fb491acfa55349a7
ZBook <izuraeva@gmail.com>
2025-01-03 21:44:43 +0300

    Mod_19_ex_1

--- ORM_Project/task1/models.py
+++ ORM_Project/task1/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    age = models.IntegerField()
+
+class Game(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    age = models.TextField(blank=True)
+    age_limited  = models.BooleanField()
+    buyer = models.ManyToManyField(Buyer, related_name='games')
+
