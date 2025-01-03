4008648b7d98003a2df38baecab7aa9bdfda40b0
ZBook <izuraeva@gmail.com>
2025-01-03 22:54:12 +0300

    Mod_19_ex_1

--- ORM_Project/task1/models.py
+++ ORM_Project/task1/models.py
@@ -18,7 +18,13 @@ class Game(models.Model):
 
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.DecimalField(max_digits=10, decimal_places=2)
-    age = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     age_limited  = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
 
+'''
+Buyer.objects.create(name= 'Serge', balance = 1500.05, age = 22)
+Buyer.objects.create(name= 'Rosaria', balance = 42.15, age = 62)
+Buyer.objects.create(name= 'Rasor', balance = 0.5, age = 12)
+ 
+'''
